d6824534337068d93161d647230666628e3e0f32
Tasnim Munawar Rafee <75935748+Rafee104104@users.noreply.github.com>
2024-10-06 18:30:43 +0600

    Update K_Max_and_Min_Codeforces.cpp

--- K_Max_and_Min_Codeforces.cpp
+++ K_Max_and_Min_Codeforces.cpp
@@ -8,12 +8,10 @@ Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/K
 #include <stdlib.h>
 
 int main() {
-    int ar[3],Max=-1000000,Min=1000000;
+    int ar[3],Max=-100000,Min=100000;
     scanf("%d %d %d",&ar[0],&ar[1],&ar[2]);
     for(unsigned short int i=0;i<3;i++){
       if(ar[i]>Max)Max=ar[i];
-    }
-    for(unsigned short int i=0;i<3;i++){
       if(ar[i]<Min)Min=ar[i];
     }
     printf("%d %d",Min,Max);  
